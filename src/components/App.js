4b5989179ac1bfd4dfa73e2ea84eecc98a939611
elfas <kibetelfas17@gmail.com>
2025-04-16 19:09:48 +0300

    Assignment completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,17 @@
 import React from "react";
-import Article from './Article'
-import Header from './Header'
+import Article from "./Article";
+import Header from "./Header";
 
 function App() {
-  return <div className="App">
-    learn react!
-    <Header/>
-    <Article />
-  </div>;
+  return (
+    <div className="App">
+      learn react!
+      <Header />
+      const element = screen.queryByText("please pass this test");
+      screen.debug(element);
+      <Article />
+    </div>
+  );
 }
 
 export default App;
